8755a70295535bb274954ba966bfc006677eccca
Milena Kuna <milena.z.kuna@gmail.com>
2024-10-30 19:23:10 +0100

    diode brightness control thru semihosting

--- LAB2/training2/Core/Src/main.c
+++ LAB2/training2/Core/Src/main.c
@@ -5,7 +5,7 @@
   * @brief          : Main program body
   ******************************************************************************
   * @attention
-  *
+  *		diode brightness control thru semihosting
   * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
